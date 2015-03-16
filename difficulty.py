dec75fcabfeaa09de490fa2b244765cf49ca1ee9
gumarus <a.avila.1@live.com>
2015-03-16 11:55:49 -0500

    pointed flask to index

--- difficulty.py
+++ difficulty.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 @app.route("/")
-def difficulty():
+def difficulty() :
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == "__main__" :
+    app.run()
