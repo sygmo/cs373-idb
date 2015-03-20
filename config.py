fc57d23b1a338f91383a90259c148dcce87891d0
gumarus <a.avila.1@live.com>
2015-03-20 17:02:19 -0500

    added some files to test backend

---
+++ config.py
@@ -0,0 +1,6 @@
+# default config
+
+class BaseConfig(object):
+	DEBUG = False
+	SQLALCHEMY_DATABASE_URI = 'static/starlordsdb.sqlite'
+	
---
+++ db_create.py
@@ -0,0 +1,5 @@
+from app import db
+from models import Base
+
+#creates the database
+db.createAll()
