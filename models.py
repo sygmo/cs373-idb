de636cdfa961b631aa2d01ef50f893d3e5e05bee
gumarus <a.avila.1@live.com>
2015-03-24 10:07:31 -0500

    updated the sqlalchemy tables

--- models.py
+++ models.py
@@ -10,16 +10,14 @@ from difficulty import db
 class family(db.Model):
 	__tablename__ = "family"
 	id = db.Column(Integer, primary_key = True)
-	name = db.Column(String(250), nullable = False)
+	name = db.Column(Text, nullable = False)
 
 class constellation(db.Model):
 	__tablename__ = "constellation"
 	id = db.Column(Integer, primary_key = True)
 	name = db.Column(Text, nullable = False)
-	area = db.Column(Float)
 	stars_with_planets = db.Column(Integer)
-	brightest_star = db.Column(Text)
-	nearest_star = db.Column(Text)
+	meaning = db.Column(Text)
 	history = db.Column(Text)
 	photo_link = db.Column(Text)
 	photo = db.Column(LargeBinary)
@@ -30,16 +28,15 @@ class planet(db.Model):
 	__tablename__ = "planet"
 	id = db.Column(Integer, primary_key = True)
 	name = db.Column(Text, nullable = False)
-	radius = db.Column(Float)
-	rings = db.Column(Integer)
 	distance_from_sun = db.Column(Float)
 	composition = db.Column(Text)
-	is_dwarf = db.Column(Boolean)
 	orbital_period = db.Column(Float)
 	volume = db.Column(Float)
 	mass = db.Column(Float)
 	density = db.Column(Float)
 	surface_area = db.Column(Float)
+	semi_major_axis = db.Column(Float)
+	rings = db.Column(Integer)
 	gravity = db.Column(Float)
 	length_of_day = db.Column(Float)
 	length_of_year = db.Column(Float)
@@ -57,16 +54,12 @@ class star(db.Model):
 	name = db.Column(Text, nullable = False)
 	mass = db.Column(Float)
 	radius = db.Column(Float)
+	spectral_type = db.Column(Text)
 	temperature = db.Column(Float)
 	luminosity = db.Column(Float)
-	surface_gravity = db.Column(Float)
 	stellar_distance = db.Column(Float)
-	stellar_classification = db.Column(Text)
-	henry_draper_catalogue = db.Column(Integer)
-	hipparcos_catalogue = db.Column(Integer)
-	apparent_magnitude = db.Column(Float)
-	absolute_magnitude = db.Column(Float)
 	parallax = db.Column(Float)	
+	planetary_systems = db.Column(Integer)	
 	photo_link = db.Column(Text)
 	history = db.Column(Text)
 	photo = db.Column(LargeBinary)
@@ -86,6 +79,20 @@ class moon(db.Model):
 	photo_link = db.Column(Text)
 	photo = db.Column(LargeBinary)
 	fk_planet_moon = db.Column(Integer, ForeignKey("planet.id"))
+
+class ExoPlanet(db.Model):
+	__tablename__ = "ExoPlanet"
+	id = db.Column(Integer, primary_key = True)
+	name = db.Column(Text, nullable = False)
+	discovered = db.Column(Text)
+	orbital_period = db.Column(Float)
+	semi_major_axis = db.Column(Float)
+	discovery_method = db.Column(Text)
+	history = db.Column(Text)
+	photo_link = db.Column(Text)
+	photo = db.Column(LargeBinary)
+	fk_star_planet = db.Column(Integer, ForeignKey("star.id"))
+
 	
 
 
--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 from unittest import main, TestCase
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 from models import family
