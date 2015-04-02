b9001ba279df6d7206c5e364093a7103923c9968
sygmo <smercier91@wireless-10-147-154-119.public.utexas.edu>
2015-04-02 15:50:59 -0500

    added ParseData folder

---
+++ ParseData/src/GetData.java
@@ -0,0 +1,29 @@
+import org.jsoup.select.Elements;
+
+
+public class GetData {
+	public static void getPlanets()
+	{
+		
+	}
+	
+	public static void getStars()
+	{
+		// http://en.wikipedia.org/wiki/List_of_exoplanetary_host_stars
+	}
+	
+	public static void getMoons()
+	{
+		
+	}
+	
+	public static void getConstellations()
+	{
+		// http://en.wikipedia.org/wiki/88_modern_constellations
+	}
+	
+	public static void getFamilies(Elements e)
+	{
+		
+	}
+}
---
+++ ParseData/src/WriteToJson.java
@@ -0,0 +1,24 @@
+import org.jsoup.select.Elements;
+
+
+public class WriteToJson {
+	public static void getPlanets()
+	{
+		
+	}
+	
+	public static void getStars()
+	{
+		// http://en.wikipedia.org/wiki/List_of_exoplanetary_host_stars
+	}
+	
+	public static void getMoons()
+	{
+		
+	}
+	
+	public static void getConstellations()
+	{
+		// http://en.wikipedia.org/wiki/88_modern_constellations
+	}
+}
