7adc2439d70e5b7fb95c51671f988c6c3a71f4c6
Olivia Lin <olivialin@wireless-10-147-205-216.public.utexas.edu>
2015-04-02 20:40:31 -0500

    removed config.py in starlords, correct config.py in cs373-idb still exists

--- starlords/config.py
+++
@@ -1,9 +0,0 @@
-# default config
-
-class BaseConfig(object):
-	DEBUG = False
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:cs373@localhost/universe'
-
-class TestConfig(object):
-	DEBUG = False
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///testdb.db'
