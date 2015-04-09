4a228d86241ad6495c9a6c0d8c0204260b3f1960
Olivia Lin <olivialin@wireless-10-147-219-36.public.utexas.edu>
2015-04-09 17:45:34 -0500

    add tests for api

---
+++ testsapi.py
@@ -0,0 +1,56 @@
+from unittest import main, TestCase
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restless import APIManager
+from flask import request
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Float, LargeBinary, Boolean
+
+from helpers import DatabaseTestBase
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+#creates the database
+db.reflect()
+db.drop_all()
+db.create_all()
+
+class family(db.Model):
+    __tablename__ = "family"
+    id = db.Column(Integer, primary_key = True)
+    name = db.Column(Text, nullable = False)
+    description = db.Column(Text)
+
+class tests(TestCase):
+
+    def test_api_path1(self):
+        with app.test_request_context('/planet'):
+            assert request.path == '/planet'
+
+    def test_api_path2(self):
+        with app.test_request_context('/constellation'):
+            assert request.path == '/constellation'
+
+    def test_api_path3(self):
+        with app.test_request_context('/family'):
+            assert request.path == '/family'
+
+    def test_api_path4(self):
+        with app.test_request_context('/star'):
+            assert request.path == '/star'
+
+    def test_api_path5(self):
+        with app.test_request_context('/moon'):
+            assert request.path == '/moon'
+
+    def test_api_path6(self):
+        with app.test_request_context('/ExoPlanet'):
+            assert request.path == '/ExoPlanet'
+
+    def test_api_get(self):
+        with app.test_request_context('/planet', method='GET'):
+            assert request.method == 'GET'
+
+if __name__ == "__main__":
+    main()
