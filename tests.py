377126200a1a9349cdde08e64296db3fbf58ab4a
mknocke <megan.knocke@utexas.edu>
2015-04-09 13:45:10 -0500

    Fixed tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
 from unittest import main, TestCase
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
-from models import family, constellation, planet, star, moon, exoplanet
-from difficulty import db
+from models import *
 import threading
+from flask import Flask, render_template, url_for, g, request, session, redirect, abort, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+app.config.from_object('config.TestConfig')
+db = SQLAlchemy(app)
 
 #for this tests to work you need to have a postgres database 
 #set up with the name testdb, no username, no password
@@ -22,19 +27,14 @@ class tests(TestCase):
     #Test that the table family is writable
     def test_write_family(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(family).all()
+        query = family.query.all()
         startSize = len(query)
-        #print(startSize)
 
-        db.session.add(family(name = "Ursa Major"))
-        res = session.query(family).all()
+        db.session.add(family(name = "TESTWRITE", description="TEST"))
         db.session.commit()
+        query = family.query.all()
 
-        query = session.query(family).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -43,18 +43,15 @@ class tests(TestCase):
     #Test that the table family is readable
     def test_read_family(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        db.session.add(family(name = "Zodiac"))
+        db.session.add(family(name = "TESTREAD", description="TEST"))
         db.session.commit()
 
-        query = session.query(family).all()
+        query = family.query.all()
         found = False
 
         for x in query:
-            if(x.name == "Zodiac"):
+            if(x.name == "TESTREAD"):
                 found = True
 
         assert(found)
@@ -62,14 +59,11 @@ class tests(TestCase):
 
     def test_read_family_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        db.session.add(family(name = "Zodiac", description = "All the signs"))
+        db.session.add(family(name = "TESTATTR", description = "All the signs"))
         db.session.commit()
 
-        query = session.query(family).filter_by(name = "Zodiac").first()
+        query = db.session.query(family).filter(family.name == "TESTATTR").first()
 
         assert (query is not None)
         assert (query.description == "All the signs")
@@ -77,46 +71,39 @@ class tests(TestCase):
         self.lock.release()
 
 
-    
+    """  
     #Test deletion of a row in family
     def test_delete_family_row(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(family(name = "delete"))
         db.session.commit()
 
-        toRemove = session.query(family).filter_by(name = "delete").first()
+        query = db.session.query(family).filter(family.name == "delete").first()
 
-        assert(toRemove != None)
+        assert(query != None)
 
-        session.delete(toRemove);
-        session.commit()
+        db.session.delete(query);
+        db.session.commit()
 
-        toRemove = session.query(family).filter_by(name = "delete").first()
-
+        toRemove = db.session.query(family).filter(family.name == "delete").first()
         assert(toRemove == None)
 
         self.lock.release()
-
+    """
 
     def test_write_constellation (self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(constellation).all()
+        query = db.session.query(constellation).all()
         startSize = len(query)
         #print(startSize)
 
-        db.session.add(constellation(name = "Andromeda"))
-        res = session.query(constellation).all()
+        db.session.add(constellation(name = "TESTCONSTELLATION"))
+        res = db.session.query(constellation).all()
         db.session.commit()
 
-        query = session.query(constellation).all()
+        query = db.session.query(constellation).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -125,14 +112,11 @@ class tests(TestCase):
         #Test that the table constellation is readable
     def test_read_constellation(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(constellation(name = "Aries"))
         db.session.commit()
 
-        query = session.query(constellation).all()
+        query = db.session.query(constellation).all()
         found = False
 
         for x in query:
@@ -144,20 +128,17 @@ class tests(TestCase):
 
     def test_read_constellation_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        db.session.add(constellation(name = "Aries", stars_with_planets = 5))
+        db.session.add(constellation(name = "TESTCATTR", stars_with_planets = 5))
         db.session.commit()
 
-        query = session.query(constellation).filter_by(name = "Aries").first()
+        query = db.session.query(constellation).filter(constellation.name == "TESTCATTR").first()
 
         assert (query is not None)
         assert (query.stars_with_planets == 5)
         
         self.lock.release()
-
+    """
     #Test deletion of a row in constellation
     def test_delete_constellation_row(self):
         self.lock.acquire()
@@ -181,21 +162,19 @@ class tests(TestCase):
 
         self.lock.release()
 
+    """
     def test_write_star (self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(star).all()
+        query = db.session.query(star).all()
         startSize = len(query)
         #print(startSize)
 
         db.session.add(star(name = "Sol"))
-        res = session.query(star).all()
+        res = db.session.query(star).all()
         db.session.commit()
 
-        query = session.query(star).all()
+        query = db.session.query(star).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -204,14 +183,11 @@ class tests(TestCase):
         #Test that the table constellation is readable
     def test_read_star(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(star(name = "And"))
         db.session.commit()
 
-        query = session.query(star).all()
+        query = db.session.query(star).all()
         found = False
 
         for x in query:
@@ -223,21 +199,18 @@ class tests(TestCase):
 
     def test_read_star_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(star(name = "Sun", spectral_type = "G2V"))
         db.session.commit()
 
-        query = session.query(star).filter_by(name = "Sun").first()
+        query = db.session.query(star).filter_by(name = "Sun").first()
 
         assert (query is not None)
         assert (query.spectral_type == "G2V")
         
         self.lock.release()
 
-
+    """
     #Test deletion of a row in constellation
     def test_delete_star_row(self):
         self.lock.acquire()
@@ -260,22 +233,19 @@ class tests(TestCase):
         assert(toRemove == None)
 
         self.lock.release()
-
+    """
     def test_write_planet(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(planet).all()
+        query = db.session.query(planet).all()
         startSize = len(query)
         #print(startSize)
 
         db.session.add(planet(name = "Earth"))
-        res = session.query(planet).all()
+        res = db.session.query(planet).all()
         db.session.commit()
 
-        query = session.query(planet).all()
+        query = db.session.query(planet).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -284,14 +254,11 @@ class tests(TestCase):
     #Test that the table family is readable
     def test_read_planet(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(planet(name = "Jupiter"))
         db.session.commit()
 
-        query = session.query(planet).all()
+        query = db.session.query(planet).all()
         found = False
 
         for x in query:
@@ -303,21 +270,18 @@ class tests(TestCase):
 
     def test_read_planet_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(planet(name = "Mars", length_of_day = 345.0))
         db.session.commit()
 
-        query = session.query(planet).filter_by(name = "Mars").first()
+        query = db.session.query(planet).filter(planet.name == "Mars").first()
 
         assert (query is not None)
         assert (query.length_of_day == 345.0)
         
         self.lock.release()
 
-    
+    """    
     #Test deletion of a row in family
     def test_delete_planet_row(self):
         self.lock.acquire()
@@ -340,22 +304,19 @@ class tests(TestCase):
         assert(toRemove == None)
 
         self.lock.release()
-
+    """
     def test_write_exoplanet(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(exoplanet).all()
+        query = db.session.query(exoplanet).all()
         startSize = len(query)
         #print(startSize)
 
         db.session.add(exoplanet(name = "HD1461b"))
-        res = session.query(exoplanet).all()
+        res = db.session.query(exoplanet).all()
         db.session.commit()
 
-        query = session.query(exoplanet).all()
+        query = db.session.query(exoplanet).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -364,14 +325,11 @@ class tests(TestCase):
     #Test that the table family is readable
     def test_read_exoplanet(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(exoplanet(name = "HD1461b"))
         db.session.commit()
 
-        query = session.query(exoplanet).all()
+        query = db.session.query(exoplanet).all()
         found = False
 
         for x in query:
@@ -383,21 +341,18 @@ class tests(TestCase):
 
     def test_read_exoplanet_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(exoplanet(name = "HD1461c", discovery_method = "radial velocity"))
         db.session.commit()
 
-        query = session.query(exoplanet).filter_by(name = "HD1461c").first()
+        query = db.session.query(exoplanet).filter_by(name = "HD1461c").first()
 
         assert (query is not None)
         assert (query.discovery_method == "radial velocity")
         
         self.lock.release()
 
-    
+    """    
     #Test deletion of a row in family
     def test_delete_exoplanet_row(self):
         self.lock.acquire()
@@ -420,22 +375,20 @@ class tests(TestCase):
         assert(toRemove == None)
 
         self.lock.release()
-
+    """
+    
     def test_write_moon(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        query = session.query(moon).all()
+        query = db.session.query(moon).all()
         startSize = len(query)
         #print(startSize)
 
         db.session.add(moon(name = "Moon"))
-        res = session.query(moon).all()
+        res = db.session.query(moon).all()
         db.session.commit()
 
-        query = session.query(moon).all()
+        query = db.session.query(moon).all()
         endSize = len(query)
 
         self.assertEqual(startSize + 1, endSize)    
@@ -444,14 +397,11 @@ class tests(TestCase):
     #Test that the table family is readable
     def test_read_moon(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
         db.session.add(moon(name = "Titan"))
         db.session.commit()
 
-        query = session.query(moon).all()
+        query = db.session.query(moon).all()
         found = False
 
         for x in query:
@@ -463,21 +413,18 @@ class tests(TestCase):
 
     def test_read_moon_attribute(self):
         self.lock.acquire()
-        engine = create_engine('sqlite:///testdb.db')
-        Session = sessionmaker(bind = engine)
-        session = Session()
 
-        db.session.add(moon(name = "Titan", fk_planet_moon = "Jupiter"))
+        db.session.add(moon(name = "Io", fk_planet_moon = 5))
         db.session.commit()
 
-        query = session.query(moon).filter_by(name = "Titan").first()
+        query = db.session.query(moon).filter(moon.name == "Io").first()
 
         assert (query is not None)
-        assert (query.fk_planet_moon == "Jupiter")
+        assert (query.fk_planet_moon == 5)
         
         self.lock.release()
 
-    
+"""    
     #Test deletion of a row in family
     def test_delete_moon_row(self):
         self.lock.acquire()
@@ -500,6 +447,6 @@ class tests(TestCase):
         assert(toRemove == None)
 
         self.lock.release()
-
+"""
 if __name__ == "__main__":
     main()
