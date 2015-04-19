8d501bc25e7537e210b317573b3054c152fb02f6
mknocke <megan.knocke@utexas.edu>
2015-04-19 18:22:49 -0500

    Got search bar to redirect to search page

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,21 @@ $(document).ready(function(){
 					"bFilter": false
                  });
 			});
+
+			
+$(function() {
+    $('#btn').click(function() {
+		var param = "search";
+        var query = {"search" : $('#srch').val()};
+        $.ajax({
+            type: 'GET',
+			url: "/search",
+			cache: false,
+            data: query,
+			datatype: "jsonp",
+            success: function(response) {
+                console.log(response);
+            }
+        });
+    });
+});
